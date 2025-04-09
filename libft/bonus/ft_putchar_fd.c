45f4eff011b67ffab6ccb48f6067ea040b426f90
gubusre <g96busquets@gmail.com>
2025-04-10 00:26:13 +0200

    Delete libft/bonus directory

--- libft/bonus/ft_putchar_fd.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-void    ft_putchar_fd(char c, int fd)
-{
-    /* first parameter is the file descriptor
-     * second parameter is the address to the character
-     */
-    write(fd, &c, 1);
-}
